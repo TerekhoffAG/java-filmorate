5689b4024e490d64d3da180b6fb3061b05044574
Terekhoff Sasha <terekhoff82@mail.ru>
2023-03-14 01:26:08 +0400

    feat: добавление контроллера фильма и пользователя

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int idGenerator = 0;
+
+    @PostMapping()
+    public User createUser(@Valid @RequestBody User user) {
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        user.setId(++idGenerator);
+        users.put(user.getId(), user);
+        log.info("Создан пользователь с id=" + user.getId());
+
+        return user;
+    }
+
+    @PutMapping()
+    public User updateUser(@Valid @RequestBody User user) {
+        if (users.containsKey(user.getId())) {
+            if (user.getName() == null) {
+                user.setName(user.getLogin());
+            }
+            users.put(user.getId(), user);
+            log.info("Обновлён пользователь с id=" + user.getId());
+        } else {
+            log.warn("Не найден пользователь с id=" + user.getId());
+            throw new NotFoundException(HttpStatus.BAD_REQUEST, "Не найден пользователь с id=" + user.getId());
+        }
+
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
