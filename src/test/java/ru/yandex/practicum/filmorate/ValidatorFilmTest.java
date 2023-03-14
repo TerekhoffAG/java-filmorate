79bbf558eb00a978589d52672031a425abd56944
Terekhoff Sasha <terekhoff82@mail.ru>
2023-03-14 06:24:32 +0400

    test: додавление тестов валидации фильма и пользователя

---
+++ src/test/java/ru/yandex/practicum/filmorate/ValidatorFilmTest.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidatorFilmTest {
+    private Validator validator;
+    private Film film;
+
+    @BeforeEach
+    void beforeEach() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+        film = Film.builder()
+                .name("nisi eiusmod")
+                .description("adipisicing")
+                .releaseDate(LocalDate.of(1967, 01, 25))
+                .duration(100)
+                .build();
+    }
+
+    @Test
+    public void shouldValidFilm() {
+        Set<ConstraintViolation<Film>> actual = validator.validate(film);
+
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void shouldInvalidNameField() {
+        film.setName("");
+        Set<ConstraintViolation<Film>> actual = validator.validate(film);
+
+        assertEquals("не должно быть пустым", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidDescriptionField() {
+        film.setDescription("Пятеро друзей ( комик-группа «Шарло»), приезжают в город Бризуль. Здесь они хотят разыскать господина Огюста Куглова, который задолжал им деньги, а именно 20 миллионов. о Куглов, который за время «своего отсутствия», стал кандидатом Коломбани.");
+        Set<ConstraintViolation<Film>> actual = validator.validate(film);
+
+        assertEquals("размер должен находиться в диапазоне от 0 до 200", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidReleaseDateField() {
+        film.setReleaseDate(LocalDate.of(1890, 01, 25));
+
+        assertThrows(ValidationException.class, () -> {
+            new FilmController().createFilm(film);
+        });
+    }
+
+    @Test
+    public void shouldInvalidDurationField() {
+        film.setDuration(-200);
+        Set<ConstraintViolation<Film>> actual = validator.validate(film);
+
+        assertEquals("должно быть больше 0", actual.stream().iterator().next().getMessage());
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/ValidatorUserTest.java
@@ -0,0 +1,93 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidatorUserTest {
+    private Validator validator;
+    private User user;
+
+    @BeforeEach
+    void beforeEach() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+        user = User.builder()
+                .login("dolore")
+                .name("Nick Name")
+                .email("mail@mail.ru")
+                .birthday(LocalDate.of(1946, 8, 20))
+                .build();
+    }
+
+    @Test
+    public void shouldValidUser() {
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void shouldInvalidLoginFieldWithSpace() {
+        user.setLogin("dolore ullamco");
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertEquals("должно соответствовать \"\\S*\"", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidLoginFieldNull() {
+        user.setLogin(null);
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertEquals("не должно равняться null", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidLoginFieldEmpty() {
+        user.setLogin("");
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertEquals("не должно быть пустым", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidEmailField() {
+        user.setEmail("mail.ru");
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertEquals("должно иметь формат адреса электронной почты", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidEmailFieldEmpty() {
+        user.setEmail("");
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertEquals("не должно быть пустым", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidBirthdayField() {
+        user.setBirthday(LocalDate.now());
+        Set<ConstraintViolation<User>> actual = validator.validate(user);
+
+        assertEquals("должно содержать прошедшую дату", actual.stream().iterator().next().getMessage());
+    }
+
+    @Test
+    public void shouldInvalidNameFieldNull() {
+        user.setName(null);
+        User actualUser = new UserController().createUser(user);
+
+        assertEquals(user.getLogin(), actualUser.getName());
+    }
+}
