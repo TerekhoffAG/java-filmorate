b3e5390babe5a8fe9b03f9a7e29e779a2778452e
Terekhoff Sasha <terekhoff82@mail.ru>
2023-03-23 05:16:41 +0400

    feat: замена функционала астрактного контроллера на сервис в контроллерах приложения.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,32 +1,38 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.constant.LogMessage;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import javax.validation.Valid;
+import java.util.Collection;
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/films")
-public class FilmController extends AbstractRestController<Film> {
+public class FilmController {
+    private final FilmService service;
+
     @PostMapping()
     public Film create(@Valid @RequestBody Film film) {
         log.info(LogMessage.POST_REQUEST);
-        return createModel(film, LogMessage.CREATE_FILM);
+        return service.saveModel(film, LogMessage.CREATE_FILM);
     }
 
     @PutMapping()
     public Film update(@Valid @RequestBody Film film) {
         log.info(LogMessage.PUT_REQUEST);
-        return updateModel(film, LogMessage.UPDATE_FILM, LogMessage.NOT_FOUND_FILM);
+        return service.updateModel(film, LogMessage.UPDATE_FILM, LogMessage.NOT_FOUND_FILM);
     }
 
     @GetMapping
-    public List<Film> getAll() {
+    public Collection<Film> getAll() {
         log.info(LogMessage.GET_REQUEST);
-        return super.getAllModels();
+        return service.getAllModels();
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,32 +1,38 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.constant.LogMessage;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
+import java.util.Collection;
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
-public class UserController extends AbstractRestController<User> {
+public class UserController {
+    private final UserService service;
+
     @PostMapping()
     public User create(@Valid @RequestBody User user) {
         log.info(LogMessage.POST_REQUEST);
-        return createModel(user, LogMessage.CREATE_USER);
+        return service.saveModel(user, LogMessage.CREATE_USER);
     }
 
     @PutMapping()
     public User update(@Valid @RequestBody User user) {
         log.info(LogMessage.PUT_REQUEST);
-        return updateModel(user, LogMessage.UPDATE_USER, LogMessage.NOT_FOUND_USER);
+        return service.updateModel(user, LogMessage.UPDATE_USER, LogMessage.NOT_FOUND_USER);
     }
 
     @GetMapping
-    public List<User> getAll() {
+    public Collection<User> getAll() {
         log.info(LogMessage.GET_REQUEST);
-        return super.getAllModels();
+        return service.getAllModels();
     }
 }
