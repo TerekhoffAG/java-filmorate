eb92376b1c33cef2c6a0ee1e1983778aea84a3f6
Terekhoff Sasha <terekhoff82@mail.ru>
2023-04-15 05:28:32 +0400

    fix: помарка.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,9 +7,7 @@ import ru.yandex.practicum.filmorate.constant.ValidatorMessage;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
